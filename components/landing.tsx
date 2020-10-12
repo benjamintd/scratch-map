c2d65b0998736dd1c2cc7367c0c58d48a6a962a8
Sarah Mamy <mamysrh@gmail.com>
2020-10-12 21:31:09 +0200

    better landing

--- components/landing.tsx
+++ components/landing.tsx
@@ -9,7 +9,7 @@ interface TxtProps {
   className?: string;
 }
 
-const H2 = (props: TxtProps) => (
+const H2 = (props: React.HTMLProps<HTMLHeadingElement>) => (
   <h2
     className={classnames("font-bold text-2xl font-lora pb-6", props.className)}
   >
@@ -17,8 +17,35 @@ const H2 = (props: TxtProps) => (
   </h2>
 );
 
-const Paragraph = (props: TxtProps) => (
+const Paragraph = (props: React.HTMLProps<HTMLParagraphElement>) => (
   <p className={classnames("mb-6", props.className)}>{props.children}</p>
+);
+
+const Step = (props: React.HTMLProps<HTMLParagraphElement>) => (
+  <p className={classnames("mb-6 text-orange-800", props.className)}>
+    {props.children}
+  </p>
+);
+
+const TutorialImage = (props: React.HTMLProps<HTMLImageElement>) => (
+  <img
+    className={classnames(
+      "relative mb-6 w-full mx-auto border rounded-lg z-10 shadow-lg",
+      props.className
+    )}
+    src={props.src}
+  />
+);
+
+const ExternalLink = (props: React.HTMLProps<HTMLAnchorElement>) => (
+  <a
+    className="text-blue-800 hover:text-blue-700 underline"
+    href={props.href}
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    {props.children}
+  </a>
 );
 
 export default function Landing() {
@@ -27,18 +54,6 @@ export default function Landing() {
 
   return (
     <div className="absolute h-full w-full overflow-y-scroll bg-gradient-to-b from-orange-100 to-orange-200 z-50 text-lg">
-      <div className="w-full bg-white h-16 shadow-md flex items-center justify-between p-4">
-        <span>
-          <span className="font-raleway mr-1">a tool from </span>
-          <span className="font-bold font-raleway text-3xl">Pelica</span>
-        </span>
-        <a
-          className="cursor-pointer"
-          href="https://github.com/benjamintd/scratch-map"
-        >
-          <img className="h-10 w-10" src="/GitHub-Mark-64px.png" />
-        </a>
-      </div>
       <div className="max-w-3xl py-12 mx-auto px-4">
         <h1 className="text-center text-5xl text-orange-800 font-lora mb-6">
           How much of the world have you <strong>scratched</strong>?
@@ -57,53 +72,94 @@ export default function Landing() {
           />
         </div>
 
-        <H2>1. Download your Google Location History</H2>
+        <H2>1. Retrieve your Google Location History</H2>
 
         <Paragraph>
           Head to{" "}
-          <a
-            className="text-blue-800 hover:text-blue-700 underline"
-            href="https://takeout.google.com"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
+          <ExternalLink href="https://takeout.google.com">
             takeout.google.com
-          </a>
+          </ExternalLink>
           , then make a download request for your location history. Unselecting
           everything except the location history will make the download faster.
         </Paragraph>
 
-        <div className="relative my-16">
-          <div className="absolute w-full h-full rounded-lg bg-orange-600 transform rotate-3 -translate-x-1 translate-y-2 z-0 shadow-lg" />
-          <img
-            className="relative w-full mb-6 mx-auto border rounded-lg z-10 shadow-lg"
-            src="/google-takeout.png"
-          />
-        </div>
-        <Paragraph>
-          Files of other formats are not supported right now.{" "}
-          <a
-            className="text-blue-800 hover:text-blue-700 underline"
-            href="mailto:benjamin.tdm@gmail.com"
-          >
-            Let us know
-          </a>{" "}
-          if you have other data that you'd like us to handle.
-        </Paragraph>
-
-        <H2>2. Find your location data file</H2>
-
         <Paragraph>
           Once the archive is ready, download it and unzip the folder. Find the
           file that is called{" "}
-          <code className="bg-gray-100 border rounded p-1 shadow-md">
-            location history.json
+          <code className="text-sm bg-gray-100 border rounded p-1 shadow-md">
+            Location History.json
           </code>
           . This file may be named differently depending on your location and
-          locale, but it's the one located at the root of the folder.
+          locale, but it's the one located at the root of the namesake folder.
         </Paragraph>
 
-        <H2>3. Drag and drop it here</H2>
+        <details className="bg-gray-100 mb-6">
+          <summary className="mb-3">
+            Need some guidance? Follow this step by step tutorial
+          </summary>
+          <div className="px-4 py-1">
+            <Paragraph>
+              Google Takeout lists your Google data sources and enables you to
+              export that data. Go to{" "}
+              <ExternalLink href="https://takeout.google.com">
+                takeout.google.com
+              </ExternalLink>{" "}
+              and log in to your Google account.
+            </Paragraph>
+            <Step>A. First, uneselect all possible exports</Step>
+            <Paragraph>
+              Click on the "Deselect All" button. You will only need your
+              location data.
+            </Paragraph>
+            <TutorialImage src="/google-deselect.png" />
+            <Step>
+              B. Head to the Location History section and select the report
+            </Step>
+            <Paragraph>
+              Scroll or look for the Location History Section. Then, click on
+              the checkbox to select this report.
+            </Paragraph>
+            <TutorialImage src="/google-location.png" />
+            <Step>C. Proceed to the next step</Step>
+            <Paragraph>
+              Scroll down the page and click on the "Next step" button
+            </Paragraph>
+            <TutorialImage src="/google-next.png" />
+            <Step>D. Export and settings</Step>
+            <Paragraph>
+              Choose how you want the export to be delivered (for ex: email). We
+              recommend using the default values (export once, .zip, 2GB).
+              Finally, click on "Create export".
+            </Paragraph>
+            <TutorialImage src="/google-settings.png" />
+            <Step>E. Extract the Location file</Step>
+            <Paragraph>
+              You will shortly receive the archive. Navigate to export it, it
+              should automatically download once you have confirmed your
+              identity and logged in again.
+            </Paragraph>
+            <TutorialImage src="/google-download.png" />
+            <Step>F. Unzip the folder and find the Location History file</Step>
+            <Paragraph>
+              Once downloaded, unzip the folder. Find the file that is called{" "}
+              <code className="text-sm bg-gray-100 border rounded p-1 shadow-md">
+                Location History.json
+              </code>
+              . This file may be named differently depending on your location
+              and locale.
+            </Paragraph>
+            <TutorialImage src="/unzip.png" />
+            <Paragraph>
+              Files of other formats are not supported right now.{" "}
+              <ExternalLink href="mailto:contact@pelica.co">
+                Let us know
+              </ExternalLink>
+              if you have other data that you'd like us to handle.
+            </Paragraph>
+          </div>
+        </details>
+
+        <H2>2. Drag and drop it here</H2>
         <Paragraph>
           Drag and drop the file here. The data is processed locally and never
           leaves your computer.
@@ -116,7 +172,7 @@ export default function Landing() {
           </button>
         </Dropzone>
 
-        <H2>4. Browse the map!</H2>
+        <H2>3. Browse the map!</H2>
         <Paragraph>
           You can try zooming around the cities you have visited 🤩.
         </Paragraph>
@@ -129,14 +185,24 @@ export default function Landing() {
         </div>
 
         <Paragraph className="mt-12 text-3xl font-lora text-center">
-          Did you like this tool?{" "}
-          <a
-            className="underline hover:text-gray-800"
-            href="https://twitter.com/intent/tweet?text=#scratchmap"
-          >
+          Do you like this tool?
+          <br />
+          <ExternalLink href="https://twitter.com/intent/tweet?text=%23scratchmap">
             Tweet us your screenshots!
-          </a>
+          </ExternalLink>
         </Paragraph>
+      </div>
+      <div className="w-full bg-white h-16 shadow-md flex items-center justify-between p-4">
+        <span>
+          <span className="font-raleway mr-1">a tool from </span>
+          <span className="font-bold font-raleway text-3xl">Pelica</span>
+        </span>
+        <a
+          className="cursor-pointer"
+          href="https://github.com/benjamintd/scratch-map"
+        >
+          <img className="h-10 w-10" src="/GitHub-Mark-64px.png" />
+        </a>
       </div>
     </div>
   );
